8331f4ebc47664a17d6647bcae8a4c4103b5c332
Yeong, Anthony <Anthony.Yeong@sc.com>
2019-02-18 11:08:23 +0800

    Add unit test

---
+++ src/test/java/com/circleci/demojavaspring/DemoJavaSpringApplication2Test.java
@@ -0,0 +1,12 @@
+package com.circleci.demojavaspring;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DemoJavaSpringApplication2Test {
+
+    @Test
+    public void main() {
+    }
+}
